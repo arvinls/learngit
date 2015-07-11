5888309e71fd4449935f747aa1fc2450f4e2cead
arvinls <liuxing_8810@126.com>
2015-07-11 08:34:42 +0800

    create a ajax file

---
+++ ajax.js
@@ -0,0 +1,27 @@
+//ajax
+function success(text){
+	var textarea = document.getElementById('test-response-text');
+	textarea.value = text;
+}
+function fail(code){
+	var textarea = document.getElementById('test-response-text');
+	textarea.value = 'Error code': code;
+}
+
+var request = new XMLHttpRequest();
+request.onreadystatechange = function(){
+	if(request.readyState === 4){
+		if (request.status === 200){
+			return success(request.responseText);
+		}else{
+			return fail(request.status);
+		}
+	}else{
+
+	}
+};
+
+request.open('GET', '/api/categories');
+request.send();
+
+alert('request has been send, please wait for response')
